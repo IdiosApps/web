b1bac7698d09e2926e72a7991e851bacb2a348a8
Alex Cabal <alex@alexcabal.com>
2022-03-18 12:59:05 -0400

    fixup! work

--- www/webhooks/postmark.php
+++ www/webhooks/postmark.php
@@ -1,32 +1,50 @@
 <?
 require_once('Core.php');
 
+use function Safe\curl_exec;
+use function Safe\curl_init;
+use function Safe\curl_setopt;
 use function Safe\file_get_contents;
 use function Safe\json_decode;
 
-$data = json_decode(file_get_contents('php://input') ?: '', true);
+$webhook = json_decode(file_get_contents('php://input') ?: '');
 
-if(!$data || !property_exists($data, 'RecordType')){
-	header(400);
+if(!$webhook || !property_exists($webhook, 'RecordType')){
+	http_response_code(400);
 	exit();
 }
 
-if($data->RecordType == 'SpamComplaint' && property_exists($data, 'Email')){
-	Db::Query('delete from NewsletterSubscribers where Email = ?', [$data->Email]);
+if($webhook->RecordType == 'SpamComplaint'){
+	Db::Query('delete from NewsletterSubscribers where Email = ?', [$webhook->Email]);
 }
 
-if($data->RecordType == 'SubscriptionChange' && property_exists($data, 'MessageId')){
-	// Get the message, and thus the email, of the subscriber change
+// Received when a user clicks Postmark's "Unsubscribe" link in a newsletter email
+if($webhook->RecordType == 'SubscriptionChange' && $webhook->SuppressSending){
+	// Get the message this subscriber change refers to, so that we can see their email address
 	$handle = curl_init();
-	curl_setopt($handle, CURLOPT_URL, 'https://api.postmarkapp.com/messages/outbound/' . $data->MessageId . '/details');
-	curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept' => 'application/json', 'X-Postmark-Server-Token' => EMAIL_SMTP_USERNAME]);
-	$message = json_decode(curl_exec($handle), true);
+	curl_setopt($handle, CURLOPT_URL, 'https://api.postmarkapp.com/messages/outbound/' . $webhook->MessageID . '/details');
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($handle, CURLOPT_HTTPHEADER, ['Accept: application/json', 'X-Postmark-Server-Token: ' . EMAIL_SMTP_USERNAME]);
+	$result = curl_exec($handle);
 	curl_close($handle);
 
-	if(!$message || !property_exists($message, 'To')){
-		header(400);
+	$message = is_bool($result) ? '' : json_decode($result);
+
+	if(!$message || !property_exists($message, 'To') || sizeof($message->To) == 0){
+		http_response_code(400);
 		exit();
 	}
 
-	Db::Query('delete from NewsletterSubscribers where Email = ?', [$message->To->Email]);
+	// Remove the email from our newsletter list
+	Db::Query('delete from NewsletterSubscribers where Email = ?', [$message->To[0]->Email]);
+
+	// Remove the suppression from Postmark, since we deleted it from our own list we will never email them again anyway
+	$handle = curl_init();
+	curl_setopt($handle, CURLOPT_URL, 'https://api.postmarkapp.com/message-streams/' . $webhook->MessageStream . '/suppressions/delete');
+	curl_setopt($handle, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($handle, CURLOPT_HTTPHEADER, ['Content-Type: application/json', 'Accept: application/json', 'X-Postmark-Server-Token: ' . EMAIL_SMTP_USERNAME]);
+	curl_setopt($handle, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($handle, CURLOPT_POSTFIELDS, '{"Suppressions": [{"EmailAddress": "' . $message->To[0]->Email . '"}]}');
+	curl_exec($handle);
+	curl_close($handle);
 }
